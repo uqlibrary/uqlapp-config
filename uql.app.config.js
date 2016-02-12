2e8ee7a9a7c4fb59b31c1883b9a0da8b2e621a91
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-02-12 11:15:19 +1000

    Initial commit v0.0.0

---
+++ uql.app.config.js
@@ -0,0 +1,24 @@
+/**
+ * Author: Jan-Willem Wisgerhof <j.wisgerhof at library.uq.edu.au>
+ * Date: 13/02/2016
+ *
+ * Main UQL App config constant
+ */
+(function() {
+  'use strict';
+
+  var uqlApiUrl = uqlApiUrl || 'https://app.library.uq.edu.au/api/';
+  var uqlAppUrl = uqlAppUrl || 'https://app.library.uq.edu.au';
+  var uqlAppMock = uqlAppMock || '';
+
+  angular.module('uql.app.config', []).constant('UQL_APP_CONFIG', {
+    apiUrl: uqlApiUrl,
+    appUrl: uqlAppUrl,
+    defaultPageTitle: 'UQ Library',
+    apiFileAllowedTypes: ['image/jpeg', 'image/png', 'application/pdf'],
+    apiFileMaxSize: 3145728,
+    uqlCookieName: 'UQLID',
+    uqlTokenHeader: 'X-Uql-Token',
+    uqlIsMocked: uqlAppMock
+  });
+})();
